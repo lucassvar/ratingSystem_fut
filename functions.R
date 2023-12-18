751b19389f0a28c3a9eba1ebc9318da6e75f226d
lucasvarelamauro@gmail.com <lucasvarelamauro@gmail.com>
2023-12-18 18:06:18 -0300

    Now fut_data_extraction() has an argument to determine the rate of extraction between male and female leagues.
    e.g. if links_examined=50 and rate_sel="70/30", then 70% of the links_examined will be male leagues (35) and 30% will be female leagues (15).
    
    The amount of links are rounded

--- functions.R
+++ functions.R
@@ -6,10 +6,7 @@ library(forcats)
 
 # Function to get the match URLs from all the leagues of the requested season (default is current season)
 get_all_match_urls <- function(year = NA){
-  # If no variables are given for either of the variables then it asumes it asks for current year
-  if (is.na(year)) {
-    year <- as.numeric(format(Sys.Date(), "%Y")) + (as.numeric(format(Sys.Date(), "%m")) > 7)
-  }
+  start_time <- Sys.time()
   
   # Countries to extract data
   first_tier_countries <- c("ENG","ESP","GER","ITA","FRA","NED","POR","BEL", "ARG","BRA","MEX","USA")
@@ -33,54 +30,77 @@ get_all_match_urls <- function(year = NA){
   )
   
   # Save both groups of links in same variable
-  all_match_urls <- list(match_urls_MASC, match_urls_FEM)
+  all_match_urls <- list("male" = match_urls_MASC, "female" = match_urls_FEM)
   
-  return(all_match_urls)
+  save(all_match_urls, file = "rda/all_match_urls.rda")
+  Sys.time() - start_time
 }
 
 # Function to extract the data from a specific year
-fut_data_extraction <- function(year_sel = NA, links_sel = NA, links_examined = 1:20){
+fut_data_extraction <- function(year_sel = NA, links_sel = NA, links_examined = 1:20, rate_sel = "80/20"){
+  load("rda/all_match_URLs.rda")
   start_time <- Sys.time()
   
   # Check if at least one of the arguments was stablished when calling
-  if (missing(year_sel) && missing(links_sel)) {stop("At least one of the arguments 'year_sel' or 'links_sel' should be given.")}
+  if (missing(year_sel) && missing(links_sel)) {
+    stop("At least one of the arguments 'year_sel' or 'links_sel' should be given.")
+  }
   
-  # Load links already used if there are any
-  if (file.exists("rda/used_links.rda")) {load("rda/used_links.rda")} else {used_links <- c()}
+  # Prepare Links ------------
+  
+  # Load links already used (if there are any)
+  if (file.exists("rda/used_links.rda")) {
+    load("rda/used_links.rda")
+  } else {
+    used_links <- c()
+  }
   
   # Remove NA values from links variable
-  all_match_URLs[1] <- na.omit(all_match_URLs[1])
-  all_match_URLs[2] <- na.omit(all_match_URLs[2])
+  links_sel$male <- na.omit(all_match_URLs$male)
+  links_sel$female <- na.omit(all_match_URLs$female)
   
-  # If there's no input of links then extract the ones from the year
-  if (is.null(links_sel)) {links_sel <- get_all_match_urls(year = year_sel)}
+  # If there's no input of links extract the ones from the year requested
+  if (is.null(links_sel)) {
+    links_sel <- get_all_match_urls(year = year_sel)
+  }
   
-  # Select links that haven't been used (add a stop to the limit of links examined if it's over the amount of links available)
-  if (length(links_examined) > length(links_sel[[1]])) {
-    masc_links <- setdiff(links_sel[[1]], used_links)
+  # Filter links_selected to only have links that haven't been used
+  masc_links <- setdiff(links_sel$male, used_links)
+  fem_links <- setdiff(links_sel$female, used_links)
+  
+  # Create amount for both male and female links (based on desired examined links and relevance rates)
+  relevance_rates <- setNames((as.numeric(strsplit(rate_sel, "/")[[1]]))/100, c("male", "female"))
+  amount_male <- round(links_examined * as.numeric(relevance_rates["male"]))
+  amount_female <- round(links_examined * as.numeric(relevance_rates["female"]))
+  
+  # Select links based on desired amount (with a stop if it's over the amount of links available)
+  if (amount_male > length(masc_links)) {
+    masc_links <- masc_links[1:length(masc_links)]
   } else {
-    masc_links <- setdiff(links_sel[[1]], used_links)[links_examined]
-  } # Male links
-  if (length(links_examined) > length(links_sel[[2]])) {
-    fem_links <- setdiff(links_sel[[2]], used_links)
+    masc_links <- masc_links[1:amount_male]
+  }
+  
+  if (amount_female > length(fem_links)) {
+    fem_links <- fem_links[1:length(fem_links)]
   } else {
-    fem_links <- setdiff(links_sel[[2]], used_links)[links_examined]
-  } # Fem links
+    fem_links <- fem_links[1:amount_female]
+  }
+  
   
   # Stat types used to extract the match logs
   stat_types <- c("passing", "passing_types", "defense", "possession", "misc")
   
-  # Empty data frames to save the data
+  # Empty variables and data frames to save the data
   playersMatchLogs <- teamsMatchLogs <- c()
+  fem_sh_logs <- masc_sh_logs <- fem_ply_keeper <- masc_ply_keeper <- fem_team_keeper <- masc_team_keeper <- data.frame()
   
   
-  # Extract shooting logs ----------
+  # Shooting Logs ------------
   print("Shooting Logs")
-  fem_sh_logs <- masc_sh_logs <- data.frame()
-  tryCatch({
-    fem_sh_logs <- fb_match_shooting(fem_links) %>% mutate(Sex = "W")
-    masc_sh_logs <- fb_match_shooting(masc_links) %>% mutate(Sex = "M")
-  })
+  
+  # Extract shooting logs data and add column for sex
+  fem_sh_logs <- fb_match_shooting(fem_links) %>% mutate(Sex = "W")
+  masc_sh_logs <- fb_match_shooting(masc_links) %>% mutate(Sex = "M")
   
   # Check that neither of the data frames are empty before binding
   if (nrow(fem_sh_logs) == 0 && nrow(masc_sh_logs) == 0) {
@@ -92,7 +112,6 @@ fut_data_extraction <- function(year_sel = NA, links_sel = NA, links_examined = 
   } else {
     sh_logs <- bind_rows(fem_sh_logs, masc_sh_logs)
   }
-  
   
   # Convert columns to numeric and create new analysis columns (if sh_logs exists)
   if (exists("sh_logs")) {
@@ -168,13 +187,13 @@ fut_data_extraction <- function(year_sel = NA, links_sel = NA, links_examined = 
              Squad = gsub("\\bUtd\\b|\\bUtd\\.\\b", "United", Squad, ignore.case = TRUE))
   }
   
-  # Extract player goalkeeping data ----------
-  print("Player - Keeper")
-  fem_ply_keeper <- masc_ply_keeper <- data.frame()
-  tryCatch({
-    fem_ply_keeper <- fb_advanced_match_stats(match_url = fem_links, stat_type = "keeper", team_or_player = "player") %>% mutate(Sex = "W")
-    masc_ply_keeper <- fb_advanced_match_stats(match_url = masc_links, stat_type = "keeper", team_or_player = "player") %>% mutate(Sex = "M")
-  })
+  
+  # Player Goalkeeping ------------
+  print("Keeper - Player")
+  
+  # Extract player goalkeeping data and add column for sex
+  fem_ply_keeper <- fb_advanced_match_stats(match_url = fem_links, stat_type = "keeper", team_or_player = "player") %>% mutate(Sex = "W")
+  masc_ply_keeper <- fb_advanced_match_stats(match_url = masc_links, stat_type = "keeper", team_or_player = "player") %>% mutate(Sex = "M")
   
   # Check that neither of the data frames are empty before binding
   if (nrow(fem_ply_keeper) == 0 && nrow(masc_ply_keeper) == 0) {
@@ -188,13 +207,12 @@ fut_data_extraction <- function(year_sel = NA, links_sel = NA, links_examined = 
   }
   
   
-  # Extract team goalkeeping data ----------
-  print("Team - Keeper")
-  fem_team_keeper <- masc_team_keeper <- data.frame()
-  tryCatch({
-    fem_team_keeper <- fb_advanced_match_stats(match_url = fem_links, stat_type = "keeper", team_or_player = "team") %>% mutate(Sex = "W")
-    masc_team_keeper <- fb_advanced_match_stats(match_url = masc_links, stat_type = "keeper", team_or_player = "team") %>% mutate(Sex = "M")
-  })
+  # Team Goalkeeping ------------
+  print("Keeper - Team")
+  
+  # Extract team goalkeeping data and add column for sex
+  fem_team_keeper <- fb_advanced_match_stats(match_url = fem_links, stat_type = "keeper", team_or_player = "team") %>% mutate(Sex = "W")
+  masc_team_keeper <- fb_advanced_match_stats(match_url = masc_links, stat_type = "keeper", team_or_player = "team") %>% mutate(Sex = "M")
   
   # Check that neither of the data frames are empty before binding
   if (nrow(fem_team_keeper) == 0 && nrow(masc_team_keeper) == 0) {
@@ -208,11 +226,10 @@ fut_data_extraction <- function(year_sel = NA, links_sel = NA, links_examined = 
   }
   
   
-  # Extract player and team match logs -----------
-  # For loop to extract the match logs from each stat type
+  # Player & Team Match Logs ------------
   for (stat in stat_types) {
     # Player stats
-    print(paste("Player -", stat))
+    print(paste(stat, "Player", sep = " - "))
     fem_playerML <- masc_playerML <- data.frame()
     tryCatch({
       fem_playerML <- fb_advanced_match_stats(match_url = fem_links,
@@ -236,7 +253,7 @@ fut_data_extraction <- function(year_sel = NA, links_sel = NA, links_examined = 
     
     
     # Team stats
-    print(paste("Team -", stat))
+    print(paste(stat, "Team", sep = " - "))
     fem_teamML <- masc_teamML <- data.frame()
     tryCatch({
       fem_teamML <- fb_advanced_match_stats(match_url = fem_links,
@@ -290,8 +307,8 @@ fut_data_extraction <- function(year_sel = NA, links_sel = NA, links_examined = 
   }
   
   
-  # Now save the data frames in the rda folder (skip if the data frames were empty and bind if files exists)
-  if (!is.null(sh_logs)) {
+  # Save data (skip empty data frames) ------------
+  if (!is.null(sh_logs) | nrow(sh_logs) > 0) {
     if(file.exists("rda/sh_logs.rda")){
       new_sh_logs <- sh_logs
       load("rda/sh_logs.rda")
@@ -299,7 +316,7 @@ fut_data_extraction <- function(year_sel = NA, links_sel = NA, links_examined = 
     }
     save(sh_logs, file = "rda/sh_logs.rda")
   }
-  if (!is.null(ply_keeper)) {
+  if (!is.null(ply_keeper) | nrow(ply_keeper) > 0) {
     if(file.exists("rda/ply_keeper.rda")){
       new_ply_keeper <- ply_keeper
       load("rda/ply_keeper.rda")
@@ -307,7 +324,7 @@ fut_data_extraction <- function(year_sel = NA, links_sel = NA, links_examined = 
     }
     save(ply_keeper, file = "rda/ply_keeper.rda")
   }
-  if (!is.null(team_keeper)) {
+  if (!is.null(team_keeper) | nrow(team_keeper) > 0) {
     if(file.exists("rda/team_keeper.rda")){
       new_team_keeper <- team_keeper
       load("rda/team_keeper.rda")
@@ -315,7 +332,7 @@ fut_data_extraction <- function(year_sel = NA, links_sel = NA, links_examined = 
     }
     save(team_keeper, file = "rda/team_keeper.rda")
   }
-  if (!is.null(playersMatchLogs)) {
+  if (!is.null(playersMatchLogs) | nrow(playersMatchLogs) > 0) {
     if(file.exists("rda/playersMatchLogs.rda")){
       new_playersMatchLogs <- playersMatchLogs
       load("rda/playersMatchLogs.rda")
@@ -323,7 +340,7 @@ fut_data_extraction <- function(year_sel = NA, links_sel = NA, links_examined = 
     }
     save(playersMatchLogs, file = "rda/playersMatchLogs.rda")
   }
-  if (!is.null(teamsMatchLogs)) {
+  if (!is.null(teamsMatchLogs) | nrow(teamsMatchLogs) > 0) {
     if(file.exists("rda/teamsMatchLogs.rda")){
       new_teamsMatchLogs <- teamsMatchLogs
       load("rda/teamsMatchLogs.rda")
@@ -332,12 +349,13 @@ fut_data_extraction <- function(year_sel = NA, links_sel = NA, links_examined = 
     save(teamsMatchLogs, file = "rda/teamsMatchLogs.rda")
   }
   
+  
+  # Register Used Links ------------
+  
   # Add the links extracted to the used data frames and save it
   used_links <- unique(c(used_links, masc_links, fem_links))
   save(used_links, file = "rda/used_links.rda")
   
-  
-  # Print total running time
   Sys.time() - start_time
 }
 
@@ -346,8 +364,8 @@ update_links <- function(new_year){
   start_time <- Sys.time()
   load("rda/all_match_URLs.rda")
   new_links <- get_all_match_urls(year = new_year)
-  all_match_URLs[[1]] <- unique(c(all_match_URLs[[1]], new_links[[1]]))
-  all_match_URLs[[2]] <- unique(c(all_match_URLs[[2]], new_links[[2]]))
+  all_match_URLs[[1]] <- unique(c(new_links[[1]], all_match_URLs[[1]]))
+  all_match_URLs[[2]] <- unique(c(new_links[[2]], all_match_URLs[[2]]))
   save(all_match_URLs, file = "rda/all_match_URLs.rda")
   Sys.time() - start_time
 }
